8a59430d46ab0ddc5f71951110149631f6a0cae1
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-01 11:52:09 -0300

    feat(server): adicionando o mongoose e as rotas de task

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const taskRoutes = require('./routes/taskRoutes');
 require('dotenv').config();
 
 const app = express();
