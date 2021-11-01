59d650cc5a3660031e918912774227895a0dd9e9
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-01 12:04:53 -0300

    feat(server): adicionando as variáveis MONGO_URI e PORT e a conexão do mongoose
    conexão do mongoose com o MongoDB

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const mongoose = require('mongoose');
 const taskRoutes = require('./routes/taskRoutes');
 require('dotenv').config();
 
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/EbytrToDoList';
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect(MONGO_URI, { 
+  useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 app.use(cors());
 app.use(express.json())
