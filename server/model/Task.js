904dc05a6549d6b69b987afc4a371365af9b2164
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-02 15:20:19 -0300

    refactor:(Task model): adicionando o validador required no model Task

--- server/model/Task.js
+++ server/model/Task.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  status: String,
+  status: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Task', taskSchema);
