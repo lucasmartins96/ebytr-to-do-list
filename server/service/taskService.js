a19a8350cb041bf77bacb6cee320062e41f9d403
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-02 14:54:23 -0300

    feat(create): adicionando as validações e a função create do service

--- server/service/taskService.js
+++ server/service/taskService.js
@@ -1,7 +1,36 @@
+const Joi = require('joi');
 const TaskModel = require('../model/Task');
+const RequestError = require('../utils/RequestError');
 
 const getAll = () => TaskModel.find({});
 
+const validateFields = (payload) => {
+  const { error } = Joi.object({
+    name: Joi.string().not().empty().required(),
+    status: Joi.alternatives().try(
+      Joi.string().pattern(/em andamento/, 'em andamento'),
+      Joi.string().pattern(/pronto/, 'pronto'),
+      Joi.string().pattern(/pendente/, 'pendente'),
+    ).not().empty()
+.required(),
+  }).validate(payload);
+
+  if (error) {
+    const { details } = error;
+    const [errorDetail] = details;
+    return errorDetail.message;
+  }
+  return null;
+};
+
+const create = async (payload) => {
+  const joiErrorMessage = validateFields(payload);
+  if (joiErrorMessage) throw new RequestError('badRequest', joiErrorMessage);
+
+  return TaskModel.create(payload);
+};
+
 module.exports = {
   getAll,
+  create,
 };
