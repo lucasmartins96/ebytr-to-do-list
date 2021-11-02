1bc07231d31e0c2f3db0e4031763a71a1cea5d0b
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-02 15:16:45 -0300

    feat(server): importação do middleware de erro

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const taskRoutes = require('./routes/taskRoutes');
+const handleErrors = require('./middlewares/handleErrors');
 require('dotenv').config();
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/EbytrToDoList';
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/tasks', taskRoutes);
+app.use(handleErrors);
 
 app.listen(PORT, () => {
   console.log(`servidor rodando na porta ${PORT}`);
