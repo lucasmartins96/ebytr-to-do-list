d4b178e320ccc9b02bfcb205e24eb830b2e32dba
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-03 19:17:04 -0300

    feat(App.js): adicionando o componente App
    o componente App renderiza o texto ebytr to do list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+/* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header" />
+      <div className="main">
+        <div className="tasks">
+          ebytr to do list
+        </div>
+      </div>
+      <div className="footer" />
     </div>
   );
 }
