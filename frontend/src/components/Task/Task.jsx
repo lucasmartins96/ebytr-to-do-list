076f879bb11023b57d72e17c83170b685178b805
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-05 11:31:29 -0300

    refactor(Task.jsx): background-color dinâmico nas tarefas
    -  a tarefa tem a cor do plano de fundo de acordo com o status
    - adicionando a classe css no nome e na data de criação da tarefa

--- frontend/src/components/Task/Task.jsx
+++ frontend/src/components/Task/Task.jsx
@@ -1,12 +1,25 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+const insertBGColorByStatus = (status) => {
+  switch (status) {
+  case 'em andamento':
+    return { backgroundColor: '#ffe44d' };
+  case 'pronto':
+    return { backgroundColor: '#2FC18C' };
+  default:
+    return { backgroundColor: '#ff9999' };
+  }
+};
+
 function Task({ taskPayload: { name, createdAt, status } }) {
   return (
-    <li className="task-component">
+    <li className="task-component" style={ insertBGColorByStatus(status) }>
       <div className="task-data">
-        <div>{name}</div>
-        <div>{new Date(createdAt).toLocaleString('pt-BR')}</div>
+        <div className="task-name">{name}</div>
+        <div className="task-createdAt">
+          {new Date(createdAt).toLocaleString('pt-BR')}
+        </div>
       </div>
       <div className="task-status">{status}</div>
     </li>
