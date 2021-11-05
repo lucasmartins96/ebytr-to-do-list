92d856bb2540944585131e2de00665f7b0fa2884
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-11-04 21:13:06 -0300

    feat(Ordenar tarefas): ordenar tarefas por nome
    - useState com o valor da opção de ordenação
    - useEffect monitorando as alterações no filterTaskName
    - select com as opções 'A-Z' e 'Z-A'
    - estruturando o HTML dos filtros das tarefas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Task from './components/Task/Task';
 
 function App() {
   const [toDoList, setToDoList] = useState(null);
+  const [alphabeticalSortingType, setAlphabeticalSortingType] = useState(' ');
 
   async function fetchAPI() {
     const response = await axios.get('https://lmartins-ebytr-todolistapi.herokuapp.com/tasks')
@@ -17,12 +18,44 @@ function App() {
     fetchAPI();
   }, []);
 
+  useEffect(() => {
+    if (alphabeticalSortingType === 'A-Z') {
+      const tasksToOrdered = [...toDoList];
+      setToDoList(tasksToOrdered.sort((a, b) => a.name.localeCompare(b.name)));
+    }
+    if (alphabeticalSortingType === 'Z-A') {
+      const tasksToOrdered = [...toDoList];
+      setToDoList(tasksToOrdered.sort((a, b) => b.name.localeCompare(a.name)));
+    }
+  }, [alphabeticalSortingType]);
+
   return (
     <div className="App">
       <div className="header" />
       <div className="main">
         <div className="tasks-container">
-          <div className="button-filters" />
+          <div className="task-filters">
+            <p>Ordenar tarefas por:</p>
+            <div className="filters-group">
+              <div className="order-name">
+                <div className="label">
+                  ordem alfabética
+                </div>
+                <div>
+                  <select
+                    value={ alphabeticalSortingType }
+                    id="select-order-task-name"
+                    name="select-order-task-name"
+                    onChange={ (e) => setAlphabeticalSortingType(e.target.value) }
+                  >
+                    <option value=" "> </option>
+                    <option value="A-Z">A-Z</option>
+                    <option value="Z-A">Z-A</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
           <ul className="list-task">
             { toDoList && toDoList.map(
               (task) => <Task key={ task._id } taskPayload={ task } />,
